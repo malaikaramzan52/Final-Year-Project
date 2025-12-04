589a4a61cea6fb0900769042ca5956156b02b2dd
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-12-04 22:16:16 +0500

    Login Page Setup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage.jsx';   // ✅ FIXED
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App 
+export default App;
