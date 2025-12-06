2c6ea3b0d91025db666d98c069b0d0222cee2581
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-12-06 18:20:09 +0500

    Backend Steup

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+const express  = require("express");
+const app = express();
+
+
+//config
+
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    require("dotenv").config({
+        path:"Backend/config/.env"
+    })
+}
+
+module.exports=app;
---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+const app = require("./app");
+const connectDatabase = require("./db/Database");
+
+// Handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to uncaught exception");
+  process.exit(1);
+});
+
+// Import config only in development mode
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "./config/.env",  // <-- FIXED PATH
+  });
+}
+
+// Connect Database
+connectDatabase();
+
+// Create Server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to unhandled promise rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
